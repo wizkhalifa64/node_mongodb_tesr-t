c740cc4085ed24462bdb6e53f3242b0b113b55a4
wizkhalifa64 <sona95986@gmail.com>
2023-12-29 00:46:31 +0530

    zod added

--- src/controller/RoleController.ts
+++ src/controller/RoleController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import Roles from "../models/Role";
 import PermissionModel from "../models/Permissions";
+import { z } from "zod";
 
 export const createRole = async (req: Request, res: Response) => {
   try {
+    const roleBodySchema = z.object({
+      roleName: z.string(),
+      permissions: z.object({}).array(),
+    });
+    roleBodySchema.parse(req.body);
     const { roleName, permissions } = req.body;
     const isPresent = await PermissionModel.find({
       _id: { $in: permissions },
@@ -18,7 +24,7 @@ export const createRole = async (req: Request, res: Response) => {
     await newRole.save();
     res.status(200).json({ message: "Role created" });
   } catch (error) {
-    res.status(500).json({ message: error });
+    res.status(400).json({ message: error });
   }
 };
 
